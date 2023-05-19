1c5039ae9a9b2180bafeb46ea2da49c73c267cde
WedNiasirikii <cm7384174@gmail.com>
2023-05-19 17:35:36 -0300

    cambios esteticos y funcionalidad

--- christian3.js
+++ christian3.js
@@ -4,15 +4,62 @@ function manejarTecla(event) {
         guardarNota();
     }
 }
+var limiteElementos = 5;
 
 function guardarNota() {
     var nota = document.getElementById("editor").value;
     var notasGuardadas = document.getElementById("notasGuardadas");
+    var limiteElementos = 5
+    // Verificar el límite de elementos
+    if (notasGuardadas.children.length >= limiteElementos) {
+        var mensajeLimite = document.createElement("div");
+        mensajeLimite.textContent = "¡Límite alcanzado 9/9!";
+        notasGuardadas.appendChild(mensajeLimite);
+
+        // Desactivar el campo de entrada
+        document.getElementById("editor").disabled = true;
+
+        return; // Detener la ejecución de la función
+    }
+
+    var contenedorNota = document.createElement("div");
+    contenedorNota.classList.add("contenedor-nota");
+
     var nuevaNota = document.createElement("div");
     nuevaNota.textContent = nota;
-    notasGuardadas.appendChild(nuevaNota);
+
+    // Agregar un botón para eliminar la nota
+    var eliminarNotaBtn = document.createElement("button");
+    eliminarNotaBtn.classList.add("btn-eliminar");
+    var eliminarIcono = document.createElement("i");
+    eliminarIcono.classList.add("material-icons");
+    eliminarIcono.textContent = "delete";
+    eliminarNotaBtn.appendChild(eliminarIcono);
+    eliminarNotaBtn.addEventListener("click", function () {
+        notasGuardadas.removeChild(contenedorNota);
+
+        // Restablecer el campo de entrada si el límite no se ha alcanzado
+        if (notasGuardadas.children.length < limiteElementos) {
+            document.getElementById("editor").disabled = false;
+        }
+    });
+
+    contenedorNota.appendChild(nuevaNota);
+    contenedorNota.appendChild(eliminarNotaBtn);
+    notasGuardadas.appendChild(contenedorNota);
     document.getElementById("editor").value = ""; // Limpiar el textarea después de guardar la nota
+
+    // Desactivar el campo de entrada si se alcanzó el límite después de guardar la nota
+    if (notasGuardadas.children.length >= limiteElementos) {
+        document.getElementById("editor").disabled = true;
+    }
+
 }
+
+
+
+
+
 function eliminarUltimaNota() {
     var notasGuardadas = document.getElementById("notasGuardadas");
     var ultimaNota = notasGuardadas.lastElementChild;
@@ -24,4 +71,11 @@ function color(z) {
     var a = getComputedStyle(z);
     var b = a.backgroundImage;
     document.getElementsByTagName("BODY")[0].style.backgroundImage = b;
+}
+function reiniciarLista() {
+    var notasGuardadas = document.getElementById("notasGuardadas");
+    notasGuardadas.innerHTML = "";
+
+    // Habilitar el campo de entrada
+    document.getElementById("editor").disabled = false;
 }
