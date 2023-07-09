4b2d88c06d309af9eaf3a9e450f885c8de41632b
WedNiasirikii <cm7384174@gmail.com>
2023-07-09 15:35:37 -0300

    sonido

--- JavaScript/password.js
+++ JavaScript/password.js
@@ -1,39 +1,115 @@
-function generarContrasena() {
-    var longitud = document.getElementById("longitud").value;
-
-    if (isNaN(longitud) || longitud < 1 || longitud > 16) {
+function incrementarNumero() {
+    var pElement = document.getElementById("numero-caracteres");
+    var numero = parseInt(pElement.innerHTML);
+  
+    if (numero < 17) {
+      numero++;
+      pElement.innerHTML = numero;
+    } else {
         Swal.fire({
             icon: 'error',
-            title: 'Oops...',
-            text: 'Ingresa un número entre 1 y 16.',
-        });
-    } else {
-        var caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&*@^ñÑ-_';
-        var contrasena = '';
-
-        for (var i = 0; i < longitud; i++) {
-            var indice = Math.floor(Math.random() * caracteres.length);
-            contrasena += caracteres.charAt(indice);
-        }
-
-        document.getElementById("contrasena-generada").textContent = contrasena;
+            title: 'El valor maximo permitido es 17',
+            position: 'top-end',
+            toast: true,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+          });
     }
 }
-
-function copyText() {
-    var contrasena = document.getElementById("contrasena-generada").textContent;
-
-    // Copiar la contraseña al portapapeles
-    var auxiliar = document.createElement("input");
-    auxiliar.setAttribute("value", contrasena);
-    document.body.appendChild(auxiliar);
-    auxiliar.select();
-    document.execCommand("copy");
-    document.body.removeChild(auxiliar);
-
-    Swal.fire({
-        icon: 'success',
-        title: 'contrase' + String.fromCharCode(241) + 'a copiada',
-        text: 'La contrase' + String.fromCharCode(241) + 'a ha sido copiada al portapapeles.',
-    });
+  
+function disminuirNumero() {  
+    var pElement = document.getElementById("numero-caracteres");
+    var numero = parseInt(pElement.innerHTML);
+  
+    if (numero > 5) {
+      numero--;
+      pElement.innerHTML = numero;
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'El valor minimo permitido es 5',
+            position: 'top-end',
+            toast: true,
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+          });
+    }
 }
+function generarContrasena() {
+  var longitudElement = document.getElementById("numero-caracteres");
+  var longitud = parseInt(longitudElement.textContent);
+  var caracteres = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#$%&*@^ñÑ-_';
+  var contrasena = '';
+
+  for (var i = 0; i < longitud; i++) {
+    var indice = Math.floor(Math.random() * caracteres.length);
+    contrasena += caracteres.charAt(indice);
+  }
+
+  var passwordElement = document.getElementById("password");
+  passwordElement.textContent = contrasena;
+}
+function copiarPassword() {
+  var passwordElement = document.getElementById("password");
+  var passwordText = passwordElement.textContent;
+
+  if (passwordText.trim() !== "") {
+    navigator.clipboard.writeText(passwordText).then(function() {
+      // Copiado exitoso
+      Swal.fire({
+        icon: 'success',
+        title: 'Contrase&ntildea copiada al portapapeles',
+        position: 'top-end',
+        toast: true,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true
+      });
+    }, function() {
+      // Error al copiar
+      Swal.fire({
+        icon: 'error',
+        title: 'No se pudo copiar la contrase&ntildea',
+        position: 'top-end',
+        toast: true,
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true
+      });
+    });
+  } else {
+    // No hay contraseña generada
+    Swal.fire({
+      icon: 'error',
+      title: 'No se ha generado ninguna contrase&ntildea',
+      position: 'top-end',
+      toast: true,
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true
+    });
+  }
+}
+
+function reproducirSonido() {
+  var clickSound = document.getElementById("clickSound1");
+  clickSound.play();
+}
+function reproducirSonido2() {
+  var clickSound = document.getElementById("clickSound2");
+  clickSound.play();
+}
+function reproducirSonido3() {
+  var clickSound = document.getElementById("clickSound3");
+  clickSound.play();
+}
+
+
+
+
+
+
+
+
